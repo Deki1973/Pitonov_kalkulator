10bb6041046e1bc09101171fe071d1a93bbfde64
Deki1973 <83946856+Deki1973@users.noreply.github.com>
2022-02-04 15:27:41 +0100

    Add files via upload

---
+++ podaci.py
@@ -0,0 +1,14 @@
+class podaci:
+   memorija: float
+
+   def podaci(self):
+       global operand1
+       global operand2
+       global operator
+       global rezultat
+       global memorija
+       self.operand1 = 0
+       self.operand1 = 0
+       self.rezultat = 0
+       self.operator = ""
+       self.memorija = 0
